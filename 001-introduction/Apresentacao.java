df30c154e9dcbc969f397762b10b58e7096e5637
Dmitri Calle <hellomitrii@gmail.com>
2026-01-27 14:42:25 -0300

    added Apresentacao.java with some notes of first class

---
+++ 001-introduction/Apresentacao.java
@@ -0,0 +1,19 @@
+package aprendendo_java_2026;
+
+public class Apresentacao {
+
+	public static void main(String[] args) {
+		// this is a comment !!
+		
+		// tipos primitivos do Java
+		int nomeDaVariavel = 10; // número inteiro
+		double tipoNumeroReal = 4.02; // número com casas decimais
+		float tipoNumeroReal32bit = 4.02f;
+		char letra = 'M';
+		boolean logica = true; // é uma variável lógica, pode ser 'true' ou 'false'
+		
+		// objetos
+		String texto = "DMT !!"; // vários caracteres
+	}
+
+}
