66ee965eb4ea3adc24130c3911a35520423cbc73
Dmitri Calle <hellomitrii@gmail.com>
2026-01-28 16:39:46 -0300

    add LacoRepeticao class with "for" loop example

---
+++ 001-introduction/LacoRepeticao.java
@@ -0,0 +1,17 @@
+package aprendendo_java_2026;
+
+public class LacoRepeticao {
+
+	public static void main(String[] args) {
+		// estudando laço de repetição "for"
+		
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(i);
+			
+			if (i == 3) {
+				continue;
+			}
+		}
+
+	}
+}
