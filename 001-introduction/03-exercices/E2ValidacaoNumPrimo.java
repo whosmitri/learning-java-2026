aed7ba93851be98c6577406c3a2c495f0585bbd6
Dmitri Calle <hellomitrii@gmail.com>
2026-01-29 14:10:38 -0300

    (001 - 03): add E2ValidacaoNumPrimo class for prime validation

---
+++ 001-introduction/03-exercices/E2ValidacaoNumPrimo.java
@@ -0,0 +1,29 @@
+package aprendendo_java_2026;
+
+import java.util.Scanner;
+
+public class E2ValidacaoNumPrimo {
+
+	public static void main(String[] args) {
+		/*
+		 * Solicite ao usuário que digite um número.
+		 * Armazene o número em uma variável.
+		 * Verifique se o número é primo.
+		 * Se o número for primo, exiba a mensagem "O número é primo".
+		 * Caso contrário, exiba a mensagem "O número não é primo".
+		 */
+		
+		// variáveis
+		int num;
+		
+		// criando scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// guardando valor
+		System.out.println("Digite um número inteiro: ");
+		num = sc.nextInt();
+		
+		// lógica
+
+	}
+}
