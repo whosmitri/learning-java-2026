bc27c54b6cdad7fea1d76912519c8956ac11e9fd
whosmitri <hellomitrii@gmail.com>
2026-02-10 15:15:46 -0300

    (006): add oop in Java file studies

---
+++ 006-object_oriented_programming/04-exercices/E1InformacoesRetangulo.java
@@ -0,0 +1,93 @@
+package aprendendo_java_2026;
+
+public class E1InformacoesRetangulo {
+    /*
+    * Crie uma classe "Retângulo" que tenha como atributos
+    * a altura, a largura, a cor e o material do retângulo,
+    * e como métodos a área, volume e o perímetro.
+    */
+
+    public static class Retangulo {
+        private double altura, largura, comprimento;
+        private String cor, material;
+
+        // métodos SET
+        public void setAltura(double altura){
+            this.altura = altura;
+        }
+        public void setLargura(double largura){
+            this.largura = largura;
+        }
+        public void setComprimento(double comprimento){
+            this.comprimento = comprimento;
+        }
+        public void setCor(String cor){
+            this.cor = cor;
+        }
+        public void setMaterial(String material){
+            this.material = material;
+        }
+
+        // métodos GET
+        public double getAltura(){
+            return this.altura;
+        }
+        public double getLargura(){
+            return this.largura;
+        }
+        public double getComprimento(){
+            return this.comprimento;
+        }
+        public String getCor(){
+            return this.cor;
+        }
+        public String getMaterial(){
+            return this.material;
+        }
+
+        // métodos gerais
+        public void imprimirDados() {
+            System.out.printf("""
+                    Altura: %.2f
+                    Largura: %.2f
+                    Comprimento: %.2f
+                    Cor: %s
+                    Material: %s""", this.altura, this.largura, this.comprimento, this.cor, this.material);
+        }
+
+        public double calcArea() {
+            double area = this.altura * this.largura;
+            return area;
+        }
+
+        public double calcVolume() {
+            double volume = this.comprimento * this.altura * this.largura;
+            return volume;
+        }
+
+        public double calcPertimetro() {
+            double perimetro = 2*this.altura + 2*this.largura;
+            return perimetro;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Retangulo retangulo1 = new Retangulo();
+        retangulo1.setAltura(4);
+        retangulo1.setLargura(2);
+        retangulo1.setCor("Verde");
+        retangulo1.setMaterial("Plástico");
+
+        retangulo1.imprimirDados();
+
+        double area = retangulo1.calcArea();
+        double perimetro = retangulo1.calcPertimetro();
+        double volume = retangulo1.calcVolume();
+        System.out.printf("""
+                %n%nÁrea: %.3f
+                Perímetro: %.3f
+                Volume: %.3f""", area, perimetro, volume);
+    }
+
+}
---
+++ 006-object_oriented_programming/CaracteristicasObjetos.java
@@ -0,0 +1,91 @@
+public class CaracteristicasObjetos {
+    public static class Cachorro {
+        private String nome;
+        private int idade;
+        private String raca;
+
+
+        //GET para pegar os dados
+        //SET para colocar dados nas variaveis
+        public String getNome(){
+            return this.nome;
+        }
+        public void setNome(String nome){
+            this.nome = nome;
+        }
+
+        public int getIdade(){
+            return this.idade;
+        }
+
+        public void setIdade(int idade){
+            if (idade >0 && idade <=20){
+                this.idade = idade;
+            }else {
+                System.err.println("Idade não permitida.");
+            }
+        }
+
+        public String getRaca(){
+            return  this.raca;
+        }
+
+        public void setRaca(String raca){
+            this.raca = raca;
+        }
+
+        public void imprimirDados(){
+            System.out.printf(
+                    """
+                            Nome: %s
+                            Idade: %d
+                            Raça: %s
+                            """,nome, idade, raca
+            );
+        }
+    }
+
+    public static class Tutor {
+        private String nome;
+        private Cachorro cachorro;
+
+        public String getNome() {
+            return this.nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Cachorro getCachorro() {
+            return this.cachorro;
+        }
+
+        public void setCachorro(Cachorro cachorro){
+            this.cachorro = cachorro;
+        }
+
+        public void imprimirDados(){
+            System.out.printf("""
+                Nome do tutor: %s
+                Cão: %s
+                """, nome, cachorro.getNome());
+        }
+    }
+
+    public class Programa {
+        public static void main(String[] args) {
+            Cachorro cachorro = new Cachorro();
+            cachorro.setNome("Totó");
+            cachorro.setIdade(10);
+            cachorro.setRaca("Boxer");
+
+            Tutor tutor = new Tutor();
+            tutor.setNome("Hebe");
+            tutor.setCachorro(cachorro);
+
+            tutor.imprimirDados();
+            cachorro.imprimirDados();
+        }
+    }
+}
